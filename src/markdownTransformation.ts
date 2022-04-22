74ccb9194cfee546b94af93d5858b3ea9e53423e
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-04-22 13:51:19 +0100

    Test success: receive a text with links as input

--- src/markdownTransformation.ts
+++ src/markdownTransformation.ts
@@ -1,7 +1,17 @@
+/* eslint-disable no-unused-vars */
 class MarkdownTransformation {
   public link2Footnote(text: string): string {
+    const link2FooterRegex = /\[.*?\]\(.+?\)/g;
+    const urlRegex = /\(.+?\)/;
+    const links = text.match(link2FooterRegex);
+    if (links) {
+      text = text.replace(links[0], '[^anchor1]');
+      const urlContent = String(links[0].match(urlRegex));
+      text = text + '\n[^anchor1]: ' + urlContent.slice(1, -1);
+    }
     return text;
   }
 }
 
 export {MarkdownTransformation};
+
--- test/markdownTranformation.spec.ts
+++ test/markdownTranformation.spec.ts
@@ -1,17 +1,22 @@
 import {MarkdownTransformation} from '../src/markdownTransformation';
 
 describe('Markdown Transformation should', () => {
-  // const markdownExample = '[this book](https://codigosostenible.com)';
-  // const result = `[^anchor1]
-  //  [^anchor1]: https://codigosostenible.com`;
-
-  it('receive text as input', () => {
+  it('receive a text without links as input', () => {
     const markdownTransformation = new MarkdownTransformation();
     const exampleWithoutLinks = 'This is an example without links';
+
     expect(markdownTransformation.link2Footnote(exampleWithoutLinks))
       .toBe(exampleWithoutLinks);
   });
 
+  it('receive a text with links as input', () => {
+    const markdownTransformation = new MarkdownTransformation();
+    const exampleWithLink = '[this book](https://codigosostenible.com)';
+    const result = '[^anchor1]\n[^anchor1]: https://codigosostenible.com';
+
+    expect(markdownTransformation.link2Footnote(exampleWithLink))
+      .toBe(result);
+  });
   // it('receive md file as input', () => {
   //   const markdownTransformation = new MarkdownTransformation();
   //   // argv.slice(2)
